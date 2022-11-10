62ef12c41b7b0de4be56324307e8a76b75c3e994
NowakArtur97 <nowart97@gmail.com>
2022-11-10 10:31:54 +0100

    Copy all files from GitHub to S3 bucket

--- github_to_s3.py
+++ github_to_s3.py
@@ -9,16 +9,42 @@ print('Loading function')
 
 s3 = boto3.resource('s3')
 
-def resolve_content_type(fileName):
-    extension = fileName.rsplit('.', 1)[1]
-    match extension:
-        case 'html':
-            return "text/html"
-        case 'css':
-            return "text/css"
+urls = [
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/index.html",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/about.html",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/contact.html",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/style.css",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/simple-house-logo.png",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/simple-house-01.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/about-01.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/about-02.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/about-03.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/about-04.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/about-05.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/about-06.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/dummy-img.png",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/img-01.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/gallery/01.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/gallery/02.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/gallery/03.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/gallery/04.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/gallery/05.jpg",
+"https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/gallery/06.jpg",
+]
+
+def resolve_content_type(extension):
+    if extension == "html":
+        return "text/html"
+    elif extension == "css":
+        return "text/css"
+    elif extension == "png":
+        return "image/png"
+    else:
+        return "image/jpeg"
 
 def map_to_resource(url):
-    return GithubResource(url, resolve_content_type(url))
+    extension = url.rsplit('.', 1)[1]
+    return GithubResource(url, resolve_content_type(extension))
 
 def save_to_local(resource):
     url = resource.url
@@ -28,24 +54,26 @@ def save_to_local(resource):
     urllib.request.urlretrieve(url, filePath)
     return filePath
 
+def resolve_key(url, fileName):
+    if "/img/gallery/" in url:
+        return "img/gallery/" + fileName
+    elif "/img/" in url:
+        return "img/" + fileName
+    else:
+        return fileName
+
 def upload_to_s3(resource, filePath, bucket):
     fileName = os.path.basename(filePath)
-    s3.Object(bucket, fileName).put(Body=open(filePath, 'rb'), ContentType=resource.extension)
+    key = resolve_key(resource.url, fileName)
+    s3.Object(bucket, key).put(Body=open(filePath, 'rb'), ContentType=resource.extension)
 
 def copy_to_s3(resource, bucket):
     filePath = save_to_local(resource)
     upload_to_s3(resource, filePath, bucket)
 
 def lambda_handler(event, context):
-    urls = [
-    "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/index.html",
-    "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/about.html",
-    "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/contact.html",
-    "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/style.css",
-    # "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/simple-house-01.jpg",
-    ]
-    resources = map(map_to_resource, urls)
     bucket = "simple-house-687fffc0"
+    resources = map(map_to_resource, urls)
 
     try:
         for resource in resources:
