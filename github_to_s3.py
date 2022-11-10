623f2198e04a96ac06d0135199b77c484b272811
NowakArtur97 <nowart97@gmail.com>
2022-11-10 09:25:06 +0100

    Resolve content type based on file extension

--- github_to_s3.py
+++ github_to_s3.py
@@ -9,6 +9,16 @@ print('Loading function')
 
 s3 = boto3.resource('s3')
 
+def resolve_content_type(fileName):
+    extension = fileName.rsplit('.', 1)[1]
+    match extension:
+        case 'html':
+            return "text/html"
+        case 'css':
+            return "text/css"
+
+def map_to_resource(url):
+    return GithubResource(url, resolve_content_type(url))
 
 def save_to_local(resource):
     url = resource.url
@@ -18,25 +28,23 @@ def save_to_local(resource):
     urllib.request.urlretrieve(url, filePath)
     return filePath
 
-
 def upload_to_s3(resource, filePath, bucket):
     fileName = os.path.basename(filePath)
     s3.Object(bucket, fileName).put(Body=open(filePath, 'rb'), ContentType=resource.extension)
-    # s3.Bucket(bucket).upload_file(filePath, fileName, ExtraArgs={ContentType: "text/html"})
 
 def copy_to_s3(resource, bucket):
     filePath = save_to_local(resource)
     upload_to_s3(resource, filePath, bucket)
 
-
 def lambda_handler(event, context):
-    resources = [
-    GithubResource("https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/index.html", "text/html"),
-    GithubResource("https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/about.html", "text/html"),
-    GithubResource("https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/contact.html", "text/html"),
-    GithubResource("https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/style.css", "text/css"),
+    urls = [
+    "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/index.html",
+    "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/about.html",
+    "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/contact.html",
+    "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/style.css",
     # "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/img/simple-house-01.jpg",
     ]
+    resources = map(map_to_resource, urls)
     bucket = "simple-house-687fffc0"
 
     try:
