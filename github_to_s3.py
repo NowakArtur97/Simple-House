7feb71e2b0174ac57df2e5a1f35a2ad526de239e
NowakArtur97 <nowart97@gmail.com>
2022-11-16 13:48:07 +0100

    Find and filter all files in repository

--- github_to_s3.py
+++ github_to_s3.py
@@ -1,53 +1,43 @@
 import os
-import urllib.request
-from urllib.parse import urlparse
 import json
+import urllib.request
 import boto3
+import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 print('Loading function')
 
 s3 = boto3.resource('s3')
-page = requests.get("https://github.com/NowakArtur97/Simple-House")
-soup = BeautifulSoup(page.content, 'html.parser')
-
-baseUrl = "https://raw.githubusercontent.com/NowakArtur97/Simple-House/master/"
-urls = [
-baseUrl + "index.html",
-baseUrl + "about.html",
-baseUrl + "contact.html",
-baseUrl + "style.css",
-baseUrl + "img/simple-house-logo.png",
-baseUrl + "img/simple-house-01.jpg",
-baseUrl + "img/about-01.jpg",
-baseUrl + "img/about-02.jpg",
-baseUrl + "img/about-03.jpg",
-baseUrl + "img/about-04.jpg",
-baseUrl + "img/about-05.jpg",
-baseUrl + "img/about-06.jpg",
-baseUrl + "img/dummy-img.png",
-baseUrl + "img/img-01.jpg",
-baseUrl + "img/gallery/01.jpg",
-baseUrl + "img/gallery/02.jpg",
-baseUrl + "img/gallery/03.jpg",
-baseUrl + "img/gallery/04.jpg",
-baseUrl + "img/gallery/05.jpg",
-baseUrl + "img/gallery/06.jpg",
-]
 
 def resolve_content_type(extension):
     if extension == "html":
         return "text/html"
     elif extension == "css":
         return "text/css"
+    elif extension == "js":
+        return "text/javascript"
     elif extension == "png":
         return "image/png"
     else:
         return "image/jpeg"
 
-def map_to_resource(url):
-    extension = url.rsplit('.', 1)[1]
-    return GithubResource(url, resolve_content_type(extension))
+def map_to_resources(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    # allFiles = soup.find_all(True, class_=['js-navigation-open', 'Link--primary'])
+    allFiles = soup.find_all('a', class_='js-navigation-open Link--primary')
+    ignored = os.environ['IGNORE_FILES']
+    files = filter(lambda f: f.get_text() not in ignored, allFiles)
+    # extension = url.rsplit('.', 1)[1]
+    for f in allFiles:
+        print(f.get_text())
+    print("*************")
+    for f in files:
+        print(f.get_text())
+    return files
+    # return GithubResource(url, resolve_content_type(extension))
 
 def save_to_local(resource):
     url = resource.url
@@ -76,20 +66,17 @@ def copy_to_s3(resource, bucket):
 
 def lambda_handler(event, context):
     bucket = os.environ['BUCKET_NAME']
-    resources = map(map_to_resource, urls)
-    files = soup.find_all(class_="Box-row")
-    for f in files:
-            print(f.get_text())
+    repositoryUrl = os.environ['REPOSITORY_URL']
+    resources = map_to_resources(repositoryUrl)
 
-    try:
-        for resource in resources:
-            copy_to_s3(resource, bucket)
+    # try:
+    #     for resource in resources:
+    #         copy_to_s3(resource, bucket)
 
-    except Exception as e:
-        print("Exception on copy")
-        print(e)
-        return
-
+    # except Exception as e:
+    #     print("Exception on copy")
+    #     print(e)
+    #     return
 class GithubResource:
   def __init__(self, url, extension):
     self.url = url
